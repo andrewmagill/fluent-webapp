7542d501d45fd495aa02d6ddfa7d9f06df98d7c2
Andrew Magill <andrew.b.magill@gmail.com>
2014-01-12 17:50:35 -0600

    adding requirejs

---
+++ js/main.js
@@ -0,0 +1,7 @@
+define(function (require, exports, module) {
+	"use strict";
+	
+	exports.fn = function () {
+		window.alert("hello owl");
+	}
+});
---
+++ js/reader.js
@@ -0,0 +1,6 @@
+define(function (require, exports, module) {
+    "use strict";
+	
+	var main = require("./main");
+	main.fn();
+});
